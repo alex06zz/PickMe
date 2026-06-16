f319a9241e741685a075d548b07bf6a9283b492e
alex06zz <alex.zuzuleac@ucdconnect.ie>
2026-06-16 22:35:51 +0100

    Initial Commit

---
+++ PickMe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PickMe
+//
+//  Created by Alex Zuzuleac on 16/06/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PickMe/PickMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PickMeApp.swift
+//  PickMe
+//
+//  Created by Alex Zuzuleac on 16/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct PickMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
