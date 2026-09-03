90e7cc1ec7d0e5f9a44382bd9d66ac64fcd34e8e
alex06zz <alex.zuzuleac@ucdconnect.ie>
2026-09-03 16:58:53 +0100

    Add multi touch finger detection and coloured indicators

---
+++ PickMe/FingerPickerView.swift
@@ -0,0 +1,18 @@
+//
+//  FingerPickerView.swift
+//  PickMe
+//
+//  Created by Alex Zuzuleac on 02/09/2026.
+//
+
+import SwiftUI
+
+struct FingerPickerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FingerPickerView()
+}
